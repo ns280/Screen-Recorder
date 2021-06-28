27684fcec82439131f472413a6465602f300ae29
ns280 <86210525+ns280@users.noreply.github.com>
2021-06-27 17:20:22 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+SCREEN_SIZE = (2560,1600)
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter("output.mp4", fourcc, 1.0, (SCREEN_SIZE))
+fps = 360
+prev = 0
+while True:
+    time_elapsed = time.time() - prev
+    img = pyautogui.screenshot()
+    if time_elapsed > 17.0/fps:
+        prev = time.time()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out.write(frame)
+
+    cv2.waitKey(100)
+cv2.destroyAllWindows()
+out.release()
+
